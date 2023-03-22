7f78026b08dd34ef845d29aa9c2808fe7bafc859
Pancratzia <ortegalaura180999@gmail.com>
2023-03-22 00:22:37 -0400

    Cards modificadas y contenido agregado. Falta agregar link e imagenes

--- 05-portafolio-react/src/components/Carta.jsx
+++ 05-portafolio-react/src/components/Carta.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import '../stylesheets/Carta.css';
+import { BsGithub } from "react-icons/bs";
+import { SiNetlify } from "react-icons/si";
 
 function Carta(props) {
   return (
     <div class="col">
       <div class="card">
-        <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp" class="card-img-top"
-          alt="Hollywood Sign on The Hill" />
-        <div class="card-body">
+        <img src="https://theluxuryeditor.com/wp-content/uploads/2019/01/frank-mckenna-219857-unsplash-300x300.jpg" class="card-img-top" alt="..." />
+        <div class="card-body text-center">
           <h5 class="card-title"> {props.titulo} </h5>
-          <p class="card-text">
-            {props.contenido}
-          </p>
+          <p class="card-text"> {props.contenido} </p>
+        </div>
+        <div className="card-footer d-flex justify-content-around">
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            <BsGithub />
+          </a>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            <SiNetlify />
+          </a>
         </div>
       </div>
     </div>
--- 05-portafolio-react/src/components/ContenedorPrincipal.jsx
+++ 05-portafolio-react/src/components/ContenedorPrincipal.jsx
@@ -6,19 +6,24 @@ import Carta from "./Carta";
 function ContenedorPrincipal() {
 
   const cartas = [
-    { id: 1, titulo: 'Carta 1', contenido: 'Contenido de la carta 1' },
-    { id: 2, titulo: 'Carta 2', contenido: 'Contenido de la carta 2' },
-    { id: 3, titulo: 'Carta 3', contenido: 'Contenido de la carta 3' },
+    { id: 1, titulo: 'Testimoniales', contenido: 'Clon de los Testimonales de FreeCodeCamp con tres cartas de perfil', github: "https://github.com/Pancratzia/Proyectos-de-React/tree/master/01-testimoniales", netlify: "https://testimoniales-react-pancratzia.netlify.app/", imagen: ""},
+    { id: 2, titulo: 'Contador', contenido: 'Contador de clicks que puede incrementar de uno en uno y reiniciarse', github: "https://github.com/Pancratzia/Proyectos-de-React/tree/master/02-contador", netlify: "https://contador-react-pancratzia.netlify.app/", imagen: "" },
+    { id: 3, titulo: 'Calculadora', contenido: 'Calculadora que realiza operaciones básicas como sumar, restar, multiplicar y dividir', github: "https://github.com/Pancratzia/Proyectos-de-React/tree/master/03-calculadora", netlify: "https://calculadora-react-pancratzia.netlify.app/", imagen: "" },
+    { id: 4, titulo: 'Aplicación de Tareas', contenido: 'Aplicación de tareas que puede crear una tarea, modificarla y borrarla', github: "https://github.com/Pancratzia/Proyectos-de-React/tree/master/04-aplicacion-de-tareas", netlify: "https://aplicaciondetareas-react-pancratzia.netlify.app/", imagen: "" }
+
   ];
 
   return (
     <main>
-      <div className="contenedor row row-cols-2 g-3">
+      <div className="contenedor">
+        <div className=" row row-cols-1 row-cols-md-2 g-4">
+          {cartas.map((carta) => (
+            <Carta key={carta.id} titulo={carta.titulo} contenido={carta.contenido} />
+          ))}
+        </div>
 
-      {cartas.map((carta) => (
-        <Carta key={carta.id} titulo={carta.titulo} contenido={carta.contenido} />
-      ))}
-        
+
+
       </div>
     </main>
   );
