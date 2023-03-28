d373b2faf96da3b196891582a8bdeafd2be52725
Pancratzia <ortegalaura180999@gmail.com>
2023-03-28 19:04:11 -0400

    Desplegable de grupos de trabajo implementado

--- 05-grupos-de-trabajo/src/App.jsx
+++ 05-grupos-de-trabajo/src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css';
-import Header from './Header';
-import Footer from './Footer';
 import Employees from './Employees';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Employees />
-      <Footer />
     </div>
   );
 }
